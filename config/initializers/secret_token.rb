fb2708a80bb77cd67cbbcda8a40b911654405122
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-12-12 21:43:14 -0700

    Initial commit.
    : #infrastructure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Habitdesign::Application.config.secret_token = '820459c1d1d62500c817e94a7f1efe47cc9f762e3ac5784ce518faa38810b7a65ab3ac37d9201a85ba04c723814f9823efab5c113a367b635d263334d4555fb6'
