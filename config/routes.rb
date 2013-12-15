f2dbd55804eb4f0095a675bde3b226746a246ee2
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-12-14 19:04:03 -0700

    Partially re-add Devise redirects.
    I had to re-add some of the Devise redirects, specifically around
    destroying sessions. Made HTML a navigable format so redirects would
    be enabled. Changed the FailureApp to do full-on redirects when a
    log-in failed so that we would render the correct URL.
    
    : #devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Habitdesign::Application.routes.draw do
   resources :habits, only: [:index]
 
-  devise_for :users, controllers: {sessions: "sessions"}
+  devise_for :users, only: []
+  devise_scope :user do
+    get 'users/sign_in' => 'sessions#new', as: :new_user_session
+    post 'users/sign_in' => 'sessions#create', as: :user_session
+    get 'users/sign_out' => 'sessions#destroy', as: :destroy_user_session
+  end
 
   root :to => 'welcome#index'
 end
--- lib/prevent_devise_redirects.rb
+++ lib/prevent_devise_redirects.rb
@@ -1,6 +1,24 @@
 class PreventDeviseRedirects < Devise::FailureApp
   def respond
-    self.status = 401
-    self.content_type = 'application/json'
+    if http_auth?
+      http_auth
+    elsif warden_options[:recall]
+      custom_redirect
+    else
+      redirect
+    end
+  end
+
+  def custom_redirect
+    store_location!
+    if flash[:timedout] && flash[:alert]
+      flash.keep(:timedout)
+      flash.keep(:alert)
+    else
+      flash[:alert] = i18n_message
+    end
+
+    custom_redirect_url = "/##{redirect_url}"
+    redirect_to custom_redirect_url
   end
 end
