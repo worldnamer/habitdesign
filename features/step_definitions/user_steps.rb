4884c2c117389e8e6dce5d6b7a9921463db1dbe3
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-12-15 14:58:23 -0700

    Remove unnecessary parens in step definitions.
    This matches what Cucumber spits out when the steps are missing, and
    is lighter than using parens, which aren't really needed.
    
    : #refactor

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,25 +1,25 @@
-Given(/^I am unregistered$/) do
+Given /^I am unregistered$/ do
   logout :user
 end
 
-Given(/^I am a user$/) do
+Given /^I am a user$/ do
   @user = create(:user)
 end
 
-Given(/^I am logged-in$/) do
+Given /^I am logged-in$/ do
   @user = create(:user)
 
   login_as @user
 end
 
-Given(/^I have a habit$/) do
+Given /^I have a habit$/ do
   @user = create(:user)
   @habit = @user.habits.create(description: "description")
 
   login_as @user
 end
 
-When(/^I sign up for an account$/) do
+When /^I sign up for an account$/ do
   visit new_user_registration_path
   fill_in 'user_email', with: 'test@example.org'
   fill_in 'user_password', with: 'password'
@@ -27,7 +27,7 @@ When(/^I sign up for an account$/) do
   click_button 'Sign up'
 end
 
-When(/^I sign up again$/) do
+When /^I sign up again$/ do
   visit new_user_registration_path
   fill_in 'user_email', with: 'test@example.org'
   fill_in 'user_password', with: 'password'
